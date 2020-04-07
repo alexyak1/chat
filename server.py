9dd3189a0e64e68621fc28bc9b79244ea90d6715
Alexandr Yakimchuk <aleksandr.iakimchuk@ericsson.com>
2020-04-07 21:12:34 +0200

    init commit

---
+++ server.py
@@ -0,0 +1,69 @@
+import asyncio
+from asyncio import transports
+
+
+class ServerProtocol:
+    login: str = None
+    server: 'Server'
+    transport: transports.Transport
+
+    def __init__(self, server: 'Server'):
+        self.server = server
+
+    def data_received(self, data:bytes):
+        print(data)
+
+        decoded = data.decode()
+
+        if self.login is not None:
+            self.send_message(decoded)
+        else:
+            if decoded.startswith("login:"):
+                self.login = decoded.replace("login:", "")
+                self.transport.write(f"Hi , {self.login}!\n".encode())
+            else:
+                self.transport.write("Bad login".encode())
+
+    def connection_made(self, transport: transports.Transport):
+        self.server.clients.append(self)
+        self.transport = transport
+        print("Come new client")
+
+    def connection_lost(self, exception):
+        self.server.clients.remove(self)
+        print("Client go out")
+
+    def send_message(self, content: str):
+        message = f"{self.login}: {content}\n"
+
+        for user in self.server.clients:
+            user.transport.write()
+
+
+class Server:
+    clients: list
+
+    def __init__(self):
+        self.clients = []
+    def build_protocol(self):
+        return ServerProtocol(self)
+
+    async def start(self):
+        loop = asyncio.get_running_loop()
+
+        coroutine = await loop.create_server(
+            self.build_protocol,
+            '127.0.0.1',
+            8888
+        )
+
+        print("server is running")
+
+        await coroutine.serve_forever()
+
+process = Server()
+
+try:
+    asyncio.run(process.start())
+except KeyboardInterrupt:
+    print("Server has been stopped")
